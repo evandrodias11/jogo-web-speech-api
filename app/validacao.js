c428ac9835d1336e0c1dd0ced5d7420ddad0b9c0
Evandro Dias <65000871+evandrodias11@users.noreply.github.com>
2023-04-11 11:30:19 -0300

    init

---
+++ app/validacao.js
@@ -0,0 +1,39 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+  const numero = +chute;
+
+  if (chuteForInvalido(numero)) {
+    elementoChute.innerHTML += "<div>Valor inválido</div>";
+    return;
+  }
+
+  if (numeroMenorOuMaiorQuePermitido(numero)) {
+    elementoChute.innerHTML += `<div>Valor invalido: fale um número entre ${menorValor} e ${maiorValor} </div>`;
+    return;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+    <h2>Voce acertou !</h2>
+    <h3>O número secreto era ${numeroSecreto}</h3>
+    
+    <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>`;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
+  } else {
+    elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
+  }
+}
+
+function chuteForInvalido(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroMenorOuMaiorQuePermitido(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener("click", function (e) {
+  if (e.target.id == "jogar-novamente") {
+    window.location.reload();
+  }
+});
